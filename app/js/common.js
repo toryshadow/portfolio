184ca5d7a41876be9fb3ca284341ab54032b4b22
alex <toryshadow@gmail.com>
2016-04-12 11:16:48 +0300

    little css changes + content add

--- app/js/common.js
+++ app/js/common.js
@@ -16,6 +16,7 @@ $(function() {
 				$('h1').css({'opacity': '0'}).removeClass('animated fadeInDown');
 				$('h3').css({'opacity': '0'}).removeClass('animated fadeInUp');
 				$('p').css({'opacity': '0'}).removeClass('animated fadeIn');
+				$('.work_slide').css({'opacity': '0'}).removeClass('animated fadeInUpBig');
 			}
 		},  // This option accepts a callback function. The function will be called before the page moves.
 		afterMove: function() {
@@ -23,6 +24,12 @@ $(function() {
 			$('h1').addClass('animated fadeInDown');
 			$('h3').addClass('animated fadeInUp');
 			$('p').addClass('animated fadeIn');
+			$('.work_slide').each(function(index){
+				var ths = $(this);
+				setInterval(function() {
+					ths.addClass("animated fadeInUpBig");
+				}, 200*index);
+			});
 		},   // This option accepts a callback function. The function will be called after the page moves.
 		loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
 		keyboard: true,                  // You can activate the keyboard controls
@@ -33,27 +40,26 @@ $(function() {
 	});
 
 	$(".works-carousel").owlCarousel({
-		loop:true,
+		center: true,
 		nav:true,
+		loop: true,
 		navText: ['<i class="fa fa-caret-left"></i>','<i class="fa fa-caret-right"></i>'] ,
 		responsive:{
-			0:{
-				items:1
-			},
 			600:{
 				items:2
 			},
 			1200:{
-				items:3
+				items:4
 			}
 		}
 	});
 
 
-	$('.ws_img_cont a').magnificPopup({
+	$('.work_slide a').magnificPopup({
 		type: 'image'
 		// other options
 	});
+
 
 
 	//SVG Fallback
